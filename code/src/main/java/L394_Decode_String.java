a78c9cee8a0a4316e6b4ec0f6b398fde7c3a4e20
= <383961310@qq.com>
2018-10-09 22:20:22 +0800

    394. Decode String

--- code/src/main/java/L394_Decode_String.java
+++ code/src/main/java/L394_Decode_String.java
@@ -3,7 +3,7 @@ import java.util.Stack;
 public class L394_Decode_String {
 
     public static void main(String[] args) {
-        System.out.println(new Solution().decodeString("2[abc]3[cd]ef"));
+        System.out.println(new Solution().decodeString("11[a]"));
     }
 
     static class Solution {
@@ -11,10 +11,17 @@ public class L394_Decode_String {
             char[] chars = s.toCharArray();
             Stack<Integer> repeatTimes = new Stack<>();
             Stack<Character> characterStack = new Stack<>();
+            boolean lastIsDigit = false;
             for (char aChar : chars) {
                 if (Character.isDigit(aChar)) {
-                    repeatTimes.push(aChar - '0');
+                    if (lastIsDigit) {
+                        repeatTimes.push(repeatTimes.pop() * 10 + (aChar - '0'));
+                    } else {
+                        repeatTimes.push(aChar - '0');
+                    }
+                    lastIsDigit = true;
                 } else if (aChar == ']') {
+                    lastIsDigit = false;
                     Integer times = repeatTimes.pop();
                     String str = "";
                     Character pop = characterStack.pop();
@@ -33,6 +40,7 @@ public class L394_Decode_String {
                         characterStack.push(c);
                     }
                 } else {
+                    lastIsDigit = false;
                     characterStack.push(aChar);
                 }
             }
