0f05d90ed283640295179cd9dfdd08079dd8f20f
= <383961310@qq.com>
2018-10-10 22:57:01 +0800

    641. Design Circular Deque

---
+++ code/src/main/java/L641_Design_Circular_Deque.java
@@ -0,0 +1,109 @@
+/**
+ * Created by Administrator on 2018/10/10.
+ */
+public class L641_Design_Circular_Deque {
+
+
+    static class MyCircularDeque {
+
+
+        private int[] queue;
+        private int head;
+        private int tail;
+        private int size;
+
+        /**
+         * Initialize your data structure here. Set the size of the deque to be k.
+         */
+        public MyCircularDeque(int k) {
+            queue = new int[k + 1];
+            head = 0;
+            tail = 0;
+            size = k + 1;
+        }
+
+        /**
+         * Adds an item at the front of Deque. Return true if the operation is successful.
+         */
+        public boolean insertFront(int value) {
+            if (isFull()) {
+                return false;
+            }
+            if (isEmpty()) {
+                insertLast(value);
+                return true;
+            }
+            head = (head + size - 1) % size;
+            queue[head] = value;
+            return true;
+        }
+
+        /**
+         * Adds an item at the rear of Deque. Return true if the operation is successful.
+         */
+        public boolean insertLast(int value) {
+            if (isFull()) {
+                return false;
+            }
+            queue[tail] = value;
+            tail = (tail + 1) % size;
+            return true;
+        }
+
+        /**
+         * Deletes an item from the front of Deque. Return true if the operation is successful.
+         */
+        public boolean deleteFront() {
+            if (isEmpty()) {
+                return false;
+            }
+            head = (head + 1) % size;
+            return true;
+        }
+
+        /**
+         * Deletes an item from the rear of Deque. Return true if the operation is successful.
+         */
+        public boolean deleteLast() {
+            if (isEmpty()) {
+                return false;
+            }
+            tail = (tail + size - 1) % size;
+            return true;
+        }
+
+        /**
+         * Get the front item from the deque.
+         */
+        public int getFront() {
+            if (isEmpty()) {
+                return -1;
+            }
+            return queue[head];
+        }
+
+        /**
+         * Get the last item from the deque.
+         */
+        public int getRear() {
+            if (isEmpty()) {
+                return -1;
+            }
+            return queue[(tail + size - 1) % size];
+        }
+
+        /**
+         * Checks whether the circular deque is empty or not.
+         */
+        public boolean isEmpty() {
+            return head == tail;
+        }
+
+        /**
+         * Checks whether the circular deque is full or not.
+         */
+        public boolean isFull() {
+            return (tail + 1) % size == head;
+        }
+    }
+}
