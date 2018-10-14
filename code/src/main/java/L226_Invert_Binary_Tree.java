d6e87f6e82182f8d636ace0f079f1e74a789ab36
= <383961310@qq.com>
2018-10-14 21:24:02 +0800

    226. Invert Binary Tree

---
+++ code/src/main/java/L226_Invert_Binary_Tree.java
@@ -0,0 +1,29 @@
+import common.TreeNode;
+
+/**
+ * Created by Administrator on 2018/10/14.
+ */
+public class L226_Invert_Binary_Tree {
+
+
+    static class Solution {
+
+        /**
+         * 从上往下遍历,
+         * 也可以从下往上遍历---未实现
+         */
+        public TreeNode invertTree(TreeNode root) {
+
+            if (root == null) {
+                return null;
+            }
+            TreeNode left = root.left;
+            root.left = root.right;
+            root.right = left;
+            invertTree(root.left);
+            invertTree(root.right);
+
+            return root;
+        }
+    }
+}
