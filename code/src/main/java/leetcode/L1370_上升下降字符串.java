ca51847d59c319bab3154e32fcd28740135c2ab0
kidding_yang <383961310@qq.com>
2020-11-25 07:20:29 +0000

    add

---
+++ code/src/main/java/leetcode/L1370_上升下降字符串.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class L1370_上升下降字符串 {
+
+    public static void main(String[] args) {
+        System.out.println('a');
+    }
+
+    static class Solution {
+        public String sortString(String s) {
+            char[] charArray = s.toCharArray();
+            int[] buckets = new int[26];
+            for (char ch : charArray) {
+
+            }
+
+            StringBuilder result = new StringBuilder();
+            while (result.toString().length() != s.length()) {
+                for (char ch : charArray) {
+
+                }
+            }
+            return "";
+        }
+    }
+}
